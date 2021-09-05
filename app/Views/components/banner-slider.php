3f2b5d287251c1566119ffc4806784b9ec332743
br3-mah <nyeleti.bremah@gmail.com>
2021-09-05 02:40:01 +0200

    first setup commit

---
+++ app/Views/components/banner-slider.php
@@ -0,0 +1,39 @@
+
+  <div class="slider">
+    <ul class="slides">
+      <li>
+        
+        <img src="https://static.pexels.com/photos/1029/landscape-mountains-nature-clouds-medium.jpg"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://static.pexels.com/photos/471/nature-flowers-plant-blur-medium.jpg"> <!-- random image -->
+        <div class="caption left-align">
+          <h3>Left Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://static.pexels.com/photos/6695/summer-sun-yellow-spring-medium.jpg"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>Right Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://static.pexels.com/photos/1848/nature-sunny-red-flowers-medium.jpg"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <script>
+    $(document).ready(function(){
+      $('.slider').slider();
+    });
+  </script>
---
+++ app/Views/welcome_message.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>xChanger | Home</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+	<!--Import Google Icon Font-->
+	<!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+	<!--Import materialize.css-->
+	<!-- <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/> -->
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">   
+</head>
+<body >
+
+<!-- HEADER: MENU + HEROE SECTION -->
+	<!-- <sideNav> -->
+<ul id="slide-out" class="padding-2 sidenav">
+		<li><div class="user-view">
+			<div class="background">
+				<span class="helper-text">
+				<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+					<circle style="fill:#E21B1B;" cx="113.704" cy="113.704" r="113.704"/>
+					<path style="fill:#FFFFFF;" d="M108.256,176.096V160.8c-8.96-0.104-17.744-2.488-25.528-6.928l4.256-14.824
+						c7.448,4.416,15.928,6.808,24.584,6.936c9.928,0,16.704-4.88,16.704-12.456c0-7.088-5.512-11.656-17.176-15.912
+						c-16.544-5.992-27.256-13.552-27.256-28.208c0.304-14.12,11.28-25.704,25.36-26.776V47.2h12.92v14.64
+						c7.552,0.08,14.984,1.856,21.752,5.2l-4.256,14.504c-6.616-3.472-13.96-5.312-21.432-5.36c-11.032,0-14.968,5.672-14.968,11.032
+						c0,6.464,5.672,10.088,19.064,15.448c17.656,6.616,25.528,15.128,25.528,29.152c-0.304,14.824-11.848,26.976-26.632,28.056v16.232
+						L108.256,176.096z"/>
+					<circle style="fill:#999999;" cx="113.704" cy="398.296" r="113.704"/>
+					<path style="fill:#FFFFFF;" d="M151.896,447.632c-8.248,4.672-17.568,7.112-27.048,7.072c-15.408,0.432-30.152-6.288-39.944-18.2
+						c-6.16-7.528-10.032-16.672-11.144-26.344H62.448v-10.784h9.896v-2.832c0-2.12,0.176-4.416,0.36-6.36H62.448V379.2h11.84
+						c1.816-9.968,6.408-19.216,13.256-26.688c9.672-10.864,23.632-16.936,38.176-16.616c8.728-0.064,17.36,1.808,25.28,5.48
+						l-4.424,16.44c-6.128-3.144-12.912-4.784-19.8-4.8c-8.552-0.216-16.768,3.344-22.456,9.728c-4.032,4.672-6.72,10.352-7.776,16.44
+						h46.336v10.96H94.448c-0.304,2.048-0.424,4.12-0.36,6.184v3h48.8v10.832H96c0.784,6.392,3.424,12.424,7.6,17.328
+						c6.184,6.4,14.792,9.864,23.688,9.536c7.272-0.088,14.424-1.904,20.856-5.296L151.896,447.632z"/>
+					<circle cx="398.296" cy="256" r="113.704"/>
+					<path style="fill:#FFFFFF;" d="M389.504,315.672v-28.04h-26.2v-10.256h26.2V265.96h-26.2v-10.256h20.992l-27.88-49.208h22.344
+						l14.112,30.4c3.016,6.216,4.864,11.424,7.048,16.8h0.344c2.016-5.032,4.192-10.912,7.048-17.128l14.616-30.072h21.832l-29.36,49.208
+						h20.8v10.248h-26.4v11.424h26.4v10.248h-26.4v28.048L389.504,315.672L389.504,315.672z"/>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					<g>
+					</g>
+					</svg>
+				</span>
+			</div>
+			<!-- <a href="#user"><img class="circle" src="images/yuna.jpg"></a> -->
+			<!-- <a href="#name"><span class="white-text name">John Doe</span></a> -->
+			<!-- <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a> -->
+		</div></li>
+		<li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+		<li><a href="#!">Second Link</a></li>
+		<li><div class="divider"></div></li>
+		<li><a class="subheader">Subheader</a></li>
+		<li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+
+	</ul>
+	<a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+		<ul id="dropdown1" class="dropdown-content">
+			<li><a href="#!">one</a></li>
+			<li><a href="#!">two</a></li>
+			<li class="divider"></li>
+			<li><a href="#!">three</a></li>
+		</ul>
+		<nav>
+			<div class="nav-wrapper deep-purple accent-1">
+				<a href="#!" class="brand-logo">xCurrency</a>
+				<ul class="right hide-on-med-and-down">
+				<li><a href="sass.html">Documentation</a></li>
+				<li><a href="sass.html">Services</a></li>
+				<li><a href="sass.html">Pricing</a></li>
+				<li><a href="badges.html">API</a></li>
+				<!-- Dropdown Trigger -->
+				<li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Sign Up<i class="material-icons right">arrow_drop_down</i></a></li>
+				</ul>
+			</div>
+		</nav>
+	<div class="fixed-action-btn">
+		<a class="btn-floating btn-large red">
+			<i class="large material-icons">mode_edit</i>
+		</a>
+		<ul>
+			<li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+			<li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+			<li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+			<li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+		</ul>
+	</div>
+	<!-- </header> -->
+
+	<!-- CONTENT -->
+	<div class="slider">
+		<ul class="slides">
+			<li>
+			
+			<img src="https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/201701/MIT-Inhibitor-Model-1_0.jpg?itok=IAVolvm3"> <!-- random image -->
+			<div class="caption center-align">
+				<h3><span class="deep-orange-text darken-4 accent-3">Fresh</span> Data</h3>
+				<h5 class="light grey-text text-lighten-3">Data will Talk to you if you are willing to listen!</h5>
+			</div>
+			</li>
+			<li>
+			<img src="https://cfcdn-fc.metalsmarketreaders.com/wp-content/uploads/sites/50/2021/04/U9fJTZ.jpg"> <!-- random image -->
+			<div class="caption left-align">
+				<h3><span class="red-text">Live </span>xCurrency Rates</h3>
+				<h5 class="light grey-text text-lighten-3">Exchange without fear.</h5>
+			</div>
+			</li>
+			<li>
+			<img src="https://www.incimages.com/uploaded_files/image/1920x1080/marletopps_364481.jpg"> <!-- random image -->
+			<div class="caption right-align">
+				<h3 class="teal-text darken-4">The real value of your company</h3>
+				<h5 class="light grey-text text-lighten-3">Give value, Give value, Give value.</h5>
+			</div>
+			</li>
+			<li>
+			<img src="https://www.lokad.com/public/Upload/Support/Glossary/missing-products-in-warehouse.jpg"> <!-- random image -->
+			<div class="caption center-align">
+				<h3> <span class="pink-text darken-4">Inventory and Stock</span> Managing</h3>
+				<h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+			</div>
+			</li>
+		</ul>
+	</div>
+
+	<!-- FOOTER: DEBUG INFO + COPYRIGHTS -->
+
+<footer>
+	<div class="environment">
+
+		<p>Page rendered in {elapsed_time} seconds</p>
+
+		<p>Environment: <?= ENVIRONMENT ?></p>
+
+	</div>
+
+	<div class="copyrights">
+
+		<p>&copy; <?= date('Y') ?> CodeIgniter Foundation. CodeIgniter is open source project released under the MIT
+			open source licence.</p>
+
+	</div>
+
+</footer>
+
+<!-- SCRIPTS -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script>
+	function toggleMenu() {
+		var menuItems = document.getElementsByClassName('menu-item');
+		for (var i = 0; i < menuItems.length; i++) {
+			var menuItem = menuItems[i];
+			menuItem.classList.toggle("hidden");
+		}
+	}
+</script>
+
+<!-- -->
+<!--JavaScript at end of body for optimized loading-->
+<!-- <script type="text/javascript" src="js/materialize.min.js"></script> -->
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+ 
+<script>
+	document.addEventListener('DOMContentLoaded', function() {
+		var elems = document.querySelectorAll('.fixed-action-btn');
+		var instances = M.FloatingActionButton.init(elems, options);
+	});
+
+	// Or with jQuery
+
+	$(document).ready(function(){
+		$('.fixed-action-btn').floatingActionButton();
+	});
+
+	document.addEventListener('DOMContentLoaded', function() {
+		var elems = document.querySelectorAll('.sidenav');
+		var instances = M.Sidenav.init(elems, options);
+  	});
+
+  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+  // var collapsibleElem = document.querySelector('.collapsible');
+  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+  // Or with jQuery
+
+	$(document).ready(function(){
+		$('.sidenav').sidenav();
+	});
+	$(document).ready(function(){
+      	$('.slider').slider();
+    });
+</script>  
+</body>
+</html>
